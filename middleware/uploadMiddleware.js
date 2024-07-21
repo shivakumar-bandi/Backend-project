841cc80a3b8d2dbe3f1dea140233fcd782d4f4ff
shiva kumar <nanishiva2022001@gmail.com>
2024-07-21 09:12:12 +0530

    made changes for events

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,29 +1,16 @@
 const multer = require('multer');
 const path = require('path');
 
-// Configure multer storage
+// Set up multer for file uploads
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Directory to save the uploaded files
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
   },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}${path.extname(file.originalname)}`); // Unique file name
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
   }
 });
 
-const upload = multer({ storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB limit
-  fileFilter: (req, file, cb) => {
-    const allowedTypes = /jpeg|jpg|png|gif/;
-    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = allowedTypes.test(file.mimetype);
-
-    if (mimetype && extname) {
-      return cb(null, true);
-    } else {
-      cb(new Error('Invalid file type. Only JPG, JPEG, PNG, and GIF files are allowed.'));
-    }
-  }
- });
+const upload = multer({ storage: storage });
 
 module.exports = upload;
