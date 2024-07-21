4a5b86707126e08a34f9af7310ffedc30d375fe3
shiva kumar <nanishiva2022001@gmail.com>
2024-07-21 08:34:35 +0530

    made changes for events

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,18 +1,11 @@
-// routes/eventRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/eventController');
 const upload = require('../middleware/uploadMiddleware');
 const authMiddleware = require('../middleware/authMiddleware');
 
-
 // POST /events - Create a new event
-router.post('/events', authMiddleware, eventController.uploadMiddleware, eventController.createEvent);
-// Added middleware for file handling
-
-// POST /events - Create a new event
-router.post('/events', eventController.createEvent);
+router.post('/events', authMiddleware, upload.single('image'), eventController.createEvent);
 
 // GET /events - Retrieve all events
 router.get('/events', eventController.getAllEvents);
@@ -21,9 +14,9 @@ router.get('/events', eventController.getAllEvents);
 router.get('/events/:id', eventController.getEventById);
 
 // PUT /events/:id - Update an event by ID
-router.put('/events/:id', eventController.updateEvent);
+router.put('/events/:id', authMiddleware, upload.single('image'), eventController.updateEvent);
 
 // DELETE /events/:id - Delete an event by ID
-router.delete('/events/:id', eventController.deleteEvent);
+router.delete('/events/:id', authMiddleware, eventController.deleteEvent);
 
 module.exports = router;
