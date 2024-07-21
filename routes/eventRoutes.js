6d8e4dd43b33df72591ab804503b9b46cbad12c0
shiva kumar <nanishiva2022001@gmail.com>
2024-07-21 07:47:08 +0530

    made changes for events

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/eventController');
 const upload = require('../middleware/uploadMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 
 
 // POST /events - Create a new event
