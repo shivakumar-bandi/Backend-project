b5e211473908de088ed5933fac5160eea618a3f4
shiva kumar <nanishiva2022001@gmail.com>
2024-07-21 10:02:09 +0530

    made changes for events

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -3,12 +3,13 @@
 const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/eventController');
+const upload = require('../middleware/uploadMiddleware'); // Create a new middleware file for multer setup
 
 // Routes for managing events
-router.post('/events', eventController.createEvent); // Remove authMiddleware
-router.get('/events', eventController.getEvents);
+router.post('/events', upload.single('image'), eventController.createEvent);
+router.get('/events', eventController.getAllEvents);
 router.get('/events/:id', eventController.getEventById);
-router.put('/events/:id', eventController.updateEvent); // Remove authMiddleware
-router.delete('/events/:id', eventController.deleteEvent); // Remove authMiddleware
+router.put('/events/:id', upload.single('image'), eventController.updateEvent);
+router.delete('/events/:id', eventController.deleteEvent);
 
 module.exports = router;
