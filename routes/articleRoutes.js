da21fba09595c33210b555273cae71241be19112
shiva kumar <nanishiva2022001@gmail.com>
2024-07-23 14:55:30 +0530

    made changes for events

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { upload, createArticle, updateArticle, deleteArticle, getArticle, getAllArticles } = require('../controllers/articleController');
 
-router.post('/', upload.single('image'), createArticle);
-router.put('/:id', upload.single('image'), updateArticle);
-router.delete('/:id', deleteArticle);
-router.get('/:id', getArticle);
-router.get('/', getAllArticles);
+router.post('/articles', upload.single('image'), createArticle);
+router.put('/articles/:id', upload.single('image'), updateArticle);
+router.delete('/articles/:id', deleteArticle);
+router.get('/articles/:id', getArticle);
+router.get('/articles', getAllArticles);
 
 module.exports = router;
