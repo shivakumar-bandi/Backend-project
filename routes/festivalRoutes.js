95e028238478475bceb635781d09c69a94d781d9
shiva kumar <nanishiva2022001@gmail.com>
2024-07-24 21:55:20 +0530

    made changes for events

--- routes/festivalRoutes.js
+++ routes/festivalRoutes.js
@@ -1,15 +1,12 @@
-// routes/festivalRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const festivalController = require('../controllers/festivalController');
 const upload = require('../middleware/uploadMiddleware');
 
-
-router.post('/festival',upload.single('image'), festivalController.createFestival);
-router.get('/festival', festivalController.getAllFestivals);
-router.get('/festival/:id', festivalController.getFestivalById);
-router.put('/festival/:id',upload.single('image'), festivalController.updateFestival);
-router.delete('/festival/:id', festivalController.deleteFestival);
+router.post('/', upload.single('image'), festivalController.createFestival);
+router.get('/', festivalController.getAllFestivals);
+router.get('/:id', festivalController.getFestivalById);
+router.put('/:id', upload.single('image'), festivalController.updateFestival);
+router.delete('/:id', festivalController.deleteFestival);
 
 module.exports = router;
