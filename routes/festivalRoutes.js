d363dd392c63c97cde8b7c1f735a14f486d1454f
shiva kumar <nanishiva2022001@gmail.com>
2024-07-25 00:06:39 +0530

    made changes for events

--- routes/festivalRoutes.js
+++ routes/festivalRoutes.js
@@ -3,7 +3,17 @@ const router = express.Router();
 const festivalController = require('../controllers/festivalController');
 const upload = require('../middleware/uploadMiddleware');
 
-router.post('/', upload.single('image'), festivalController.createFestival);
+router.post('/', upload.single('image'), (req, res) => {
+    try {
+      console.log('File uploaded:', req.file);
+      res.status(200).json({ message: 'File uploaded successfully' });
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      res.status(500).json({ message: 'Error uploading file' });
+    }
+  });
+  
+
 router.get('/', festivalController.getAllFestivals);
 router.get('/:id', festivalController.getFestivalById);
 router.put('/:id', upload.single('image'), festivalController.updateFestival);
