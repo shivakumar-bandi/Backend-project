744ea28a51bfd57ff685654b45a68fc94a12a472
shiva kumar <nanishiva2022001@gmail.com>
2024-07-31 09:30:17 +0530

    Your commit message

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { upload, createArticle, updateArticle, deleteArticle, getArticle, getAllArticles } = require('../controllers/articleController');
-const upload = require('../middleware/uploadMiddleware'); // Create a new middleware file for multer setup
 
 
 router.post('/', upload.single('image'), createArticle);
