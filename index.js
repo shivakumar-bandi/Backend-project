7650fb089703b211c58f1f0ac63f3fe301ba19f8
shiva kumar <nanishiva2022001@gmail.com>
2024-08-05 21:46:30 +0530

    Your commit changes in all article component

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
-// Serve static files from the uploads directory
-app.use('/uploads', express.static('uploads'));
+
+// Serve static files from the 'uploads' directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/articles', articleRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/festivals', festivalRoutes);
