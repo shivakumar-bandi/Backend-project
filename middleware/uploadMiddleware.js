00813d30947ccedc03288b396918ae70b66d1399
shiva kumar <nanishiva2022001@gmail.com>
2024-08-06 11:05:05 +0530

    Your commit changes in article

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -3,19 +3,21 @@ const path = require('path');
 const fs = require('fs');
 
 const uploadDir = 'uploads';
+
+// Ensure the uploads directory exists
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: (req, file, cb) => {
     cb(null, uploadDir);
   },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // Append file extension
   }
 });
 
 const upload = multer({ storage: storage });
 
-module.exports = upload;
+module.exports =upload;
