681bd49e7c1768c558e2a63d7976d6af654b0a77
shiva kumar <nanishiva2022001@gmail.com>
2024-08-06 12:29:53 +0530

    Your commit changes in article

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const app = express();
 dotenv.config();
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
+
 
 app.use(cors());
 app.use(bodyParser.json());
