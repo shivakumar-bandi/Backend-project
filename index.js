0aae406814150a490bfcdf53775481ae128f745a
shiva kumar <nanishiva2022001@gmail.com>
2024-08-06 08:13:24 +0530

    Your commit changes in all article component

--- index.js
+++ index.js
@@ -1,50 +1,45 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const articleRoutes = require('./routes/articleRoutes');
 const eventRoutes = require('./routes/eventRoutes');
 const festivalRoutes = require('./routes/festivalRoutes');
-const path = require('path'); 
 
-dotenv.config();
-connectDB();
+delete require.cache[require.resolve('./models/Festival')];
 
 const app = express();
+
+dotenv.config();
+
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// Ensure this path is correct for your deployment environment
-const uploadsPath = path.join(__dirname, 'uploads');
+// Define the root path route
+app.get('/', (req, res) => {
+    res.send('<h1>Hello Radha-Krishna</h1>');
+});
 
-app.use('/uploads', express.static(uploadsPath));
+// Define other routes
+app.use('/user', userRoutes);
+app.use('/uploads', express.static('uploads'));
 app.use('/api/articles', articleRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/festivals', festivalRoutes);
 
+
+// Catch-all route for unknown paths
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
-
-app.get('/test', (req, res) => {
-  res.send('Server is working!');
-});
-
-
-app.use((req, res, next) => {
-  res.status(404).send('Cannot GET ' + req.originalUrl);
+    res.status(404).send('Cannot GET ' + req.originalUrl);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started and running at ${PORT}`);
+    console.log(`Server started and running at ${PORT}`);
 });
