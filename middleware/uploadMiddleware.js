2e0006d70d19ef160e736842343baf9482e55119
shiva kumar <nanishiva2022001@gmail.com>
2024-08-07 13:03:53 +0530

    Your commit changes in article

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -3,6 +3,12 @@ const path = require('path');
 const AWS = require('aws-sdk');
 const multerS3 = require('multer-s3');
 
+// Log environment variables
+console.log('AWS_ACCESS_KEY_ID:', process.env.AWS_ACCESS_KEY_ID);
+console.log('AWS_SECRET_ACCESS_KEY:', process.env.AWS_SECRET_ACCESS_KEY);
+console.log('AWS_REGION:', process.env.AWS_REGION);
+console.log('AWS_BUCKET_NAME:', process.env.AWS_BUCKET_NAME);
+
 // Initialize the S3 client with credentials from .env
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
